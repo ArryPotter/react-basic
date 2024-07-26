4d99511181f6e180a715c84dafdb50b39b88a189
arrypotter <superrian@gmail.com>
2024-07-26 10:23:54 +0700

    State Assessment

--- 2 my-state/src/App.jsx
+++ 2 my-state/src/App.jsx
@@ -1,39 +1,5 @@
-import { useState } from "react";
-
-
-function CoreConcept({title, description}){
-  return (
-    <li id='book-item' key = {title}>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
-
 function App() {
-  const [bookList, setBookList] = useState([]);
-  const [item, setItem] = useState({
-    title: '',
-    description: '',
-    quantity: ''
-  });
-
-
-  const addBook = () => {    
-    setBookList((prev) => [...prev, item]);
-  }
-
-  const handleChange = (event) => {
-    const field = event.target.name;
-    const value = event.target.value;   
-
-    setItem((prev) => ({
-        ...prev, 
-        [field]: value
-      })
-    )
-  }
-
+ 
   return (
     <div>
       <header>
@@ -45,17 +11,15 @@ function App() {
       <main>
         <section id='input-data'> 
             <div id='myInput'>
-              <div><p>Title </p><input name='title' onChange={(event) => handleChange(event)}></input></div>     
-              <div><p>Description </p><input name='description' onChange={(event) => handleChange(event)}></input> </div>         
-              <div><p>Quantity </p><input name='quantity' onChange={(event) => handleChange(event)}></input></div>
+              <div><p>Title </p><input name='title'></input></div>     
+              <div><p>Description </p><input name='description'></input> </div>         
+              <div><p>Quantity </p><input name='quantity'></input></div>
             </div>            
-            <div><button onClick={()=>{addBook()}}>Submit</button>
+            <div><button>Submit</button>
             </div>                     
         </section>
         <section id='book-collection'>
-          <ul>
-            {bookList.map(book => <CoreConcept title={book.title} description={book.description} />)}
-          </ul>
+         {/* please put the book list in here */}
         </section>
       </main>
     </div>
